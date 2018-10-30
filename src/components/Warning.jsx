dbf91e8e9104acc6d11b49cdbc1dbf4b9f0eb72f
석상환 <bdoi16@naver.com>
2018-10-30 16:43:09 +0900

    warning 페이지 수정

--- src/components/Warning.jsx
+++ src/components/Warning.jsx
@@ -17,8 +17,8 @@ export default class Warning extends React.Component {
     return (
       <div className="warning-page">
         <div>
-          <p>잘못된 접근입니다.<br />
-          잠시만 기다려주세요 채널리스트로 돌아가겠습니다.</p>
+          <p>잘못된 접근입니다.<br />잠시만 기다려주세요 채널리스트로 돌아가겠습니다.</p>
+          <p className="import">회의 중 갑자기 통신이 끊어지게되는 것은<br />주최지가 채널을 삭제해서입니다.</p>
         </div>
         <div className="loadspin"></div>
         {
